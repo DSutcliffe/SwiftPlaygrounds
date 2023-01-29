eb0ec8fc0ddeb9225ad5d7baac46b97248bd3db1
Daniel Sutcliffe <daniel.sutcliffe@co-operativebank.co.uk>
2023-01-29 16:03:23 +0000

    Refactor

--- Playground.playground/Pages/ifLet.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/ifLet.xcplaygroundpage/Contents.swift
@@ -1,5 +1,21 @@
-var optionalName: String? = "John Appleseed"    // set to nil
-var greeting = "Hello!"
-if let name = optionalName {
-    greeting = "Hello, \(name)"
+/// ************************************************************************************************************************************
+/// 01. If Let contains a value
+/// ************************************************************************************************************************************
+var optionalName1: String? = "John Appleseed"
+var greeting1 = "Hello!"
+if let name = optionalName1 {
+    greeting1 = "Hello, \(name)"
 }
+
+print(greeting1)
+
+/// ************************************************************************************************************************************
+/// 02. If let = nil
+/// ************************************************************************************************************************************
+var optionalName2: String?
+var greeting2 = "Hello!"
+if let name = optionalName2 {
+    greeting2 = "Hello, \(name)"    // only applies value when if let is != nil
+}
+
+print(greeting2)
