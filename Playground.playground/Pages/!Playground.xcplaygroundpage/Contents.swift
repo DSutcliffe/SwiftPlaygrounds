afa2e082b42cbad30781947465fd462c43d199d0
Daniel Sutcliffe <daniel.sutcliffe@co-operativebank.co.uk>
2023-11-10 11:34:11 +0000

    Organise Alphabetically

--- Playground.playground/Pages/!Playground.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/!Playground.xcplaygroundpage/Contents.swift
@@ -1,1 +1,29 @@
-
+{
+  "_accountType": "CreditCardAccount",
+  "identifier": {
+    "_type": "CreditCard",
+    "rawIdValue": "400385XXXXXX1478"
+  },
+  "product": {
+    "productCat": "09",
+    "name": "CREDIT CARD",
+    "category": {
+      "categoryPermissions": {
+        "canDoFundTransfer": false,
+        "canDoBillPayment": false,
+        "canDoForeignPayment": false,
+        "canViewStandingOrders": false,
+        "canCreateStandingOrder": false,
+        "canCancelStandingOrder": false,
+        "canViewDirectDebits": false,
+        "canCancelDirectDebit": false,
+        "canViewPlannedPayments": false
+      }
+    }
+  },
+  "hasCard": false,
+  "onlineStatementPreferenceFlag": "P",
+  "paperlessStatementsToggleAvailable": false,
+  "paperlessStatementsEnabled": false,
+  "productCode": "0100"
+}
